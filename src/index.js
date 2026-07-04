a334b720e1736c1853c375cc6b9c373bbfe53aab
Abhiraj kumar <abhirajmait16@gmail.com>
2026-07-04 21:48:43 +0530

    HomePage Setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import HomePage from "./landing_page/home/HomePage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>react</h1>
+      <HomePage />
     </div>
-  
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
